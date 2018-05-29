53d2fe27c687ea40035ee2ccec30703d06d21aab
PatoTheBest <jpguereque@yahoo.com.mx>
2018-05-29 18:59:38 -0500

    Update to 1.12, use AsyncPlayerPreLogin for loading data

--- Bukkit/src/main/java/be/spyproof/nickmanager/listeners/PlayerListener.java
+++ Bukkit/src/main/java/be/spyproof/nickmanager/listeners/PlayerListener.java
@@ -4,9 +4,14 @@ import be.spyproof.nickmanager.controller.INicknameController;
 import be.spyproof.nickmanager.model.NicknameData;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.Optional;
 
 /**
  * Created by Spyproof on 13/11/2016.
@@ -28,11 +33,31 @@ public class PlayerListener implements Listener
      * If the player has a nickname, apply it to the player
      * @param event The fired event
      */
-    @EventHandler
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onLogin(AsyncPlayerPreLoginEvent event)
+    {
+        if(event.getLoginResult() != AsyncPlayerPreLoginEvent.Result.ALLOWED) {
+            return;
+        }
+
+        this.playerController.wrap(event.getUniqueId(), event.getName());
+
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onLogin(PlayerLoginEvent event)
+    {
+        if(event.getResult() != PlayerLoginEvent.Result.ALLOWED) {
+            this.playerController.logout(event.getPlayer().getUniqueId());
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onLogin(PlayerJoinEvent event)
     {
         NicknameData nicknameData = this.playerController.wrap(event.getPlayer().getUniqueId(), event.getPlayer().getName());
-        if (nicknameData.getNickname().isPresent())
+
+        if (nicknameData.getNickname().isPresent() && nicknameData.getNickname().isPresent())
             event.getPlayer().setDisplayName(ChatColor.translateAlternateColorCodes('&', nicknameData.getNickname().get()) + ChatColor.RESET);
     }
 
@@ -41,7 +66,7 @@ public class PlayerListener implements Listener
      * @param event The fired event
      */
     @EventHandler
-    public void onLogin(PlayerQuitEvent event)
+    public void onLogout(PlayerQuitEvent event)
     {
         this.playerController.logout(event.getPlayer().getUniqueId());
     }
